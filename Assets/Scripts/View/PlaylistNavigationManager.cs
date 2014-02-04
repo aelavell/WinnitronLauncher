31102a4b5e9bddefff48b9f24893922f008c2caf
Allan Lavell <alavell@gmail.com>
2014-02-04 16:11:14 -0400

    Basic broken playlist grid idea

---
+++ Assets/Scripts/View/PlaylistNavigationManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlaylistNavigationManager : Singleton<PlaylistNavigationManager> {
+	float GRID_Y_OFFSET = 60;
+	List<Game> playlist;
+	int currentGameIndex = 0;
+	public GameObject gameLabelPrefab;
+	Coroutine<_> moveThroughList;
+	UIGrid titleGrid;
+	
+	void Start() {
+		playlist = GameRepository.Instance.games;
+		titleGrid = GetComponentInChildren<UIGrid>();
+	}
+
+	void CreateLabels() { 
+		foreach (var game in playlist) {
+
+		}
+	}
+
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.UpArrow)) Move(-1);
+		else if(Input.GetKeyDown(KeyCode.DownArrow)) Move(1);
+	}
+
+	void Move(int direction) {
+		if (moveThroughList == null) {
+			int nextGameIndex = Maths.Mod((currentGameIndex + direction), playlist.Count);
+			Debug.Log (nextGameIndex);
+			ScreenshotDisplayManager.Instance.LoadScreenshot(playlist[nextGameIndex].screenshot);
+
+			var startY = titleGrid.transform.localPosition.y;
+			moveThroughList = StartCoroutine<_>(Coroutines.OverTime(ScreenshotDisplayManager.Instance.CROSSFADE_TIME, Funcs.Identity, t => { 
+				var lp = titleGrid.transform.localPosition;
+				titleGrid.transform.localPosition = new Vector3(lp.x, Mathf.SmoothStep(startY, startY + direction * GRID_Y_OFFSET, t), lp.z);
+				if (t == 1) {
+					currentGameIndex = Maths.Mod(currentGameIndex + direction, playlist.Count);
+					moveThroughList = null;
+				}
+			}));
+		}
+
+	}
+}
--- Assets/Scripts/View/ScreenshotDisplayManager.cs
+++ Assets/Scripts/View/ScreenshotDisplayManager.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class ScreenshotDisplayManager : MonoBehaviour {
-	float CROSSFADE_TIME = 0.5f;
+public class ScreenshotDisplayManager : Singleton<ScreenshotDisplayManager> {
+	public float CROSSFADE_TIME = 0.5f;
 	UITexture[] screens;
 	UIPanel panel;
 	int currentScreenIndex = 0;
@@ -20,25 +20,19 @@ public class ScreenshotDisplayManager : MonoBehaviour {
 		screens[0].mainTexture = GameRepository.Instance.games[0].screenshot;
 	}
 
-	float time = 0;
-	bool loaded = false;
 	void Update() {
-		if (time > 1 && !loaded) {
-			LoadScreenshot(GameRepository.Instance.games[1].screenshot);
-			loaded = true;
-		}
-		time += Time.deltaTime;
 		panel.Refresh();
 	}
 
 	public void LoadScreenshot(Texture2D screenshot) {
 		if (crossfade == null) {
-			screens[currentScreenIndex + 1].mainTexture = screenshot;
+			var nextIndex = Maths.Mod((currentScreenIndex + 1), screens.Length);
+			screens[nextIndex].mainTexture = screenshot;
 			crossfade = StartCoroutine<_>(Coroutines.OverTime(CROSSFADE_TIME, Funcs.Identity, t => { 
 				screens[currentScreenIndex].alpha = 1 -t; 
-				screens[currentScreenIndex + 1].alpha = t;
+				screens[nextIndex].alpha = t;
 				if (t == 1) {
-					currentScreenIndex = (currentScreenIndex + 1) % screens.Length;
+					currentScreenIndex = Maths.Mod((currentScreenIndex + 1), screens.Length);
 					crossfade = null;
 				}
 			}));
