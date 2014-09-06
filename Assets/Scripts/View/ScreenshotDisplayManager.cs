9e61051b1d1b3e43fcb5c6fb21a1d42baa1ce667
Ben Swinden <swinden.ben@gmail.com>
2014-09-06 14:48:31 -0400

    - started adding screenshotdisplay

--- Assets/Scripts/View/ScreenshotDisplayManager.cs
+++ Assets/Scripts/View/ScreenshotDisplayManager.cs
@@ -1,24 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class ScreenshotDisplayManager : Singleton<ScreenshotDisplayManager> {
-    //public float CROSSFADE_TIME = 0.25f;
-    //UITexture[] screens;
+
+    public float CROSSFADE_TIME = 0.25f;
+    Image[] screens;
     //UIPanel panel;
-    //int currentScreenIndex = 0;
-    //Coroutine<_> crossfade;
+    int currentScreenIndex = 0;
+    Coroutine<_> crossfade;
 
-    //void Start() {
-    //    panel = transform.parent.GetComponent<UIPanel>();
-    //    screens = GetComponentsInChildren<UITexture>();
-    //    foreach (var screen in screens) {
-    //        screen.alpha = 0;
-    //    }
+    void Start() {
 
-    //    screens[0].alpha = 1;
-    //    screens[0].mainTexture = GameRepository.Instance.games[0].screenshot;
-    //}
+        //panel = transform.parent.GetComponent<UIPanel>();
+        screens = GetComponentsInChildren<Image>();
+        
+        foreach (var screen in screens) {
+            // Need to manipulate the color property instead?
+            //screen.alpha = 0;     
+        }
+
+        //screens[0].alpha = 1;
+        //screens[0].mainTexture = GameRepository.Instance.games[0].screenshot;
+    }
 
     //void Update() {
     //    panel.Refresh();
@@ -28,8 +33,8 @@ public class ScreenshotDisplayManager : Singleton<ScreenshotDisplayManager> {
     //    if (crossfade == null) {
     //        var nextIndex = Maths.Mod((currentScreenIndex + 1), screens.Length);
     //        screens[nextIndex].mainTexture = screenshot;
-    //        crossfade = StartCoroutine<_>(Coroutines.OverTime(CROSSFADE_TIME, Funcs.Identity, t => { 
-    //            screens[currentScreenIndex].alpha = 1 -t; 
+    //        crossfade = StartCoroutine<_>(Coroutines.OverTime(CROSSFADE_TIME, Funcs.Identity, t => {
+    //            screens[currentScreenIndex].alpha = 1 - t;
     //            screens[nextIndex].alpha = t;
     //            if (t == 1) {
     //                currentScreenIndex = Maths.Mod((currentScreenIndex + 1), screens.Length);
