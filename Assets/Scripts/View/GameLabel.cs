d258d864b6625f951e2f19ea75b4b80bcd442d69
Ben Swinden <swinden.ben@gmail.com>
2014-09-08 10:27:28 -0400

    - working on movement of game labels - added GoKit package for tween functions - removed old move code from navigation manager - added move function to the gamelabel object so that each label can keep track of their own tween

--- Assets/Scripts/View/GameLabel.cs
+++ Assets/Scripts/View/GameLabel.cs
@@ -1,14 +1,50 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameLabel : MonoBehaviour {
 
+    GoTween currentTween;
+    Text text;    
+
 	public Vector2 origSize;
 	public float selectedSize = 1;
 
+    public float tweenTime;
+    
+    // Properties for the tween to manipulate, regular GoKit position tween cannot be accessed with the GameLabel object so a custom property is necessary
+    public int fontSize {
+        get { return text.fontSize; }
+        set { text.fontSize = value; }
+    }    
+    public Vector3 positionProperty{
+        get { return transform.position; }
+        set { transform.position = value; }
+    }
+    
+
 	void Awake() {
 		origSize = transform.localScale;
+
+        text = GetComponent<Text>();
+        //fontSize = text.fontSize;           // Try doing this with a direct reference to the text object
 	}
+
+    void Update() {
+
+        //print(fontSize + "   " + text.fontSize);
+    }
+
+    public void move(Vector3 pos, int size) {
+
+        currentTween = Go.to(this, tweenTime, new GoTweenConfig()
+            .vector3Prop("positionProperty", pos)
+            .intProp("fontSize", size)
+            .setEaseType(GoEaseType.ExpoOut));
+
+
+        currentTween.setOnCompleteHandler(c => { onMoveComplete(); });
+    }
 
 	public void Select() {
         //TweenScale tween = TweenScale.Begin (this.gameObject, 0.2f, transform.localScale * selectedSize) ;
@@ -19,4 +55,9 @@ public class GameLabel : MonoBehaviour {
         //TweenScale tween = TweenScale.Begin (this.gameObject, 0.2f, origSize);
         //tween.method = UITweener.Method.EaseOut;
 	}
+
+    public void onMoveComplete() {
+
+
+    }
 }
--- Assets/Scripts/View/PlaylistNavigationManager.cs
+++ Assets/Scripts/View/PlaylistNavigationManager.cs
@@ -1,40 +1,45 @@
-﻿using UnityEngine;
+﻿/*
+ * All labels move at once when moving through the grid. The selected label is placed in the middle and scaled up. Labels above and below the selected label are placed
+ * relative to that label and are then placed either above of below that
+ */ 
+
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class PlaylistNavigationManager : Singleton<PlaylistNavigationManager> {
 
-    float GRID_Y_OFFSET = 60;
     List<Game> playlist;
     int currentGameIndex = 0;
-    public Text gameLabelPrefab;
     Coroutine<_> moveThroughList;
-    GameObject titleGrid;
+
+    List<GameLabel> labelList;
+    int selectedLabelIndex = 0;
+
+    public Text gameLabelPrefab;
+    public float GRID_Y_OFFSET = 60;
+
+    // The fixed positions of labels, all other labels are placed relative to these
+    public GameObject labelAbove;
+    public GameObject labelSelected;
+    public GameObject labelBelow;
 
     void Start() {
 
-        playlist = GameRepository.Instance.games;
-        titleGrid = GameObject.Find("TitleGrid");
+        labelList = new List<GameLabel>();
+
+        playlist = GameRepository.Instance.games;        
 
         CreateLabels();
-    }
-
-    void CreateLabels() {
-
-        foreach (var game in playlist) {
-            var label = Instantiate(gameLabelPrefab) as Text;
-            label.text = game.name;
-            label.transform.parent = titleGrid.transform;
-            label.transform.localScale = new Vector3(1, 1, 1);      // Why?
-        }
+        
     }
 
     void Update() {
 
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.I)) Move(-1);
+        //if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.I)) Move(-1);
 
-        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.K)) Move(1);
+        //else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.K)) Move(1);
 
         if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.N) || Input.GetKeyDown(KeyCode.M)) {
             // only able choose the game if we're not currently moving through the game list
@@ -44,20 +49,32 @@ public class PlaylistNavigationManager : Singleton<PlaylistNavigationManager> {
         }
     }
 
-    void Move(int direction) { 
-        if (moveThroughList == null && (currentGameIndex + direction >= 0) && (currentGameIndex + direction < playlist.Count)) {
-            int nextGameIndex = Maths.Mod((currentGameIndex + direction), playlist.Count);
-            //ScreenshotDisplayManager.Instance.LoadScreenshot(playlist[nextGameIndex].screenshot);
+    void CreateLabels() {
 
-            var startY = titleGrid.transform.localPosition.y;
-            //moveThroughList = StartCoroutine<_>(Coroutines.OverTime(ScreenshotDisplayManager.Instance.CROSSFADE_TIME, Funcs.Identity, t => {
-            //    var lp = titleGrid.transform.localPosition;
-            //    titleGrid.transform.localPosition = new Vector3(lp.x, Mathf.SmoothStep(startY, startY + direction * GRID_Y_OFFSET, t), lp.z);
-            //    if (t == 1) {
-            //        currentGameIndex = Maths.Mod(currentGameIndex + direction, playlist.Count);
-            //        moveThroughList = null;
-            //    }
-            //}));
+        foreach (Game game in playlist) {
+
+            var label = Instantiate(gameLabelPrefab) as Text;
+            label.text = game.name;            
+            label.transform.parent = transform;
+            
+            labelList.Add(label.GetComponent<GameLabel>());
+        }
+
+        sortLabelList();
+    }
+
+    void sortLabelList() {
+
+        // Move and scale currently selected label        
+        labelList[selectedLabelIndex].move(labelSelected.transform.position, 36);
+
+        // Move all labels above it, starting with the closest
+        var index = selectedLabelIndex - 1;
+        var startIndex = index;
+        while (index >= 0) {
+
+            labelList[index].move(new Vector3(labelAbove.transform.position.x, labelAbove.transform.position.y + (GRID_Y_OFFSET * (startIndex - index)), labelAbove.transform.position.z), 36);
+            index--;
         }
     }
 }
