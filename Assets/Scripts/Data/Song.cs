ce3653e3f4f304722ca7cac7e65fd8114271469e
Ben Swinden <swinden.ben@gmail.com>
2014-09-13 10:50:51 -0400

    -starting song data storage

---
+++ Assets/Scripts/Data/Song.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Song {
+
+	public string name;
+	public string author;
+	public Sprite screenshot;
+	public string executablePath;
+	
+	public Song(string name, string author, Sprite screenshot, string executablePath) {
+		this.name = name;
+		this.author = author;
+		this.screenshot = screenshot;
+		this.executablePath = executablePath;
+	}
+}
