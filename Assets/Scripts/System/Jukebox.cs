40252a4209ccf6eec7ce85e7b13ec8d2b520e583
Ben Swinden <swinden.ben@gmail.com>
2014-09-13 13:26:55 -0400

    - playlist artist and song name in botton right

--- Assets/Scripts/System/Jukebox.cs
+++ Assets/Scripts/System/Jukebox.cs
@@ -4,7 +4,11 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Jukebox : MonoBehaviour {
-    
+
+    public Text artistName;
+    public Text songName;
+
+
     private Song[] songs;
 
     private int currentTrack;
@@ -20,6 +24,8 @@ public class Jukebox : MonoBehaviour {
         currentTrack = Random.Range(0, songs.Length);
         audio.clip = songs[currentTrack].clip;
         audio.Play();
+
+        initWdiget();
     }
 
     void Update() {
@@ -34,25 +40,33 @@ public class Jukebox : MonoBehaviour {
         
         audio.Stop();
 
-        if (currentTrack >= songs.Length)
+        if (currentTrack >= songs.Length - 1)
             currentTrack = 0;
         else
             currentTrack++;
 
         audio.clip = songs[currentTrack].clip;
         audio.Play();
+        initWdiget();
     }
 
     public void lastTrack() {
 
         audio.Stop();
 
-        if (currentTrack <= 0)
+        if (currentTrack <= 1)
             currentTrack = songs.Length - 1;
         else
             currentTrack--;
 
         audio.clip = songs[currentTrack].clip;
         audio.Play();
+        initWdiget();
+    }
+
+    public void initWdiget() {
+
+        songName.text = songs[currentTrack].name;
+        artistName.text = songs[currentTrack].author;
     }
 }
