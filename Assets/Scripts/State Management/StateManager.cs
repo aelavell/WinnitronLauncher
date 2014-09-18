8ed6db11a23b98cefc2e47a9323aabcf5a71a1ef
Marlon Wiebe <mr.m.wiebe@gmail.com>
2014-09-17 23:18:58 -0300

    Global launcher state and state managing.  Intro now integrated into launcher under Intro+Launcher(Marlon)

---
+++ Assets/Scripts/State Management/StateManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StateManager : MonoBehaviour {
+
+	public List<GameObject> gameObjectsToToggleActivation;
+	public List<Canvas> canvasesToToggleActivation;
+
+	//On Activate
+	public List<Animation> animationsToPlayOnStart;
+
+	//On deactivate
+	public List<Animation> animationsToRewind;
+
+	public void Deactivate() {
+		foreach(GameObject gameObject in gameObjectsToToggleActivation) gameObject.SetActive(false);
+		foreach(Canvas canvas in canvasesToToggleActivation) canvas.enabled = false;
+		foreach(Animation anim in animationsToRewind) {
+			anim.Stop();
+			anim.Rewind();
+		}
+	}
+
+	public void Activate() {
+		foreach(GameObject gameObject in gameObjectsToToggleActivation) gameObject.SetActive(true);
+		foreach(Canvas canvas in canvasesToToggleActivation) canvas.enabled = true;
+		foreach(Animation anim in animationsToPlayOnStart) anim.Play();
+	}
+}
